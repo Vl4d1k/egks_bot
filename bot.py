0954ba7fe83f89bacfae30bb1a8b4d81b0648ae4
Vlad Stelmakh <vlad1k9369@gmail.com>
2021-10-17 16:00:09 +0300

    delete warnings

--- bot.py
+++ bot.py
@@ -9,12 +9,13 @@ import logging
 import time
 import telebot
 import os
+from requests.packages import urllib3
 from dotenv import load_dotenv
 
-#todo исправить логирование
-#todo вынести проверку автора в декоратор
-#todo сделать автодеплой на сервер
-#todo добавить карточки для созранения карт
+# todo исправить логирование
+# todo вынести проверку автора в декоратор
+# todo сделать автодеплой на сервер
+# todo добавить карточки для созранения карт
 
 load_dotenv()
 
@@ -32,6 +33,12 @@ logging.basicConfig(filename=log_path,
                     datefmt='%H:%M:%S',
                     level=logging.INFO)
 
+
+def main():
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    bot_polling()
+
+
 def bot_polling():
     global bot
     logging.info('Starting bot polling now')
@@ -40,9 +47,11 @@ def bot_polling():
             logging.info('New bot instance started')
             bot = telebot.TeleBot(API_TOKEN)
             bot_actions()
-            bot.polling(none_stop=True, interval=BOT_INTERVAL, timeout=BOT_TIMEOUT)
+            bot.polling(none_stop=True, interval=BOT_INTERVAL,
+                        timeout=BOT_TIMEOUT)
         except Exception as ex:
-            logging.info(f'Bot polling failed, restarting in {BOT_TIMEOUT} sec. Error: {ex}')
+            logging.info(
+                f'Bot polling failed, restarting in {BOT_TIMEOUT} sec. Error: {ex}')
             bot.stop_polling()
             time.sleep(BOT_TIMEOUT)
         else:
@@ -50,24 +59,29 @@ def bot_polling():
             logging.info('Bot polling loop finished')
             break
 
+
 def bot_actions():
 
     @bot.message_handler(commands=['start'])
     def send_welcome(message):
-        bot.reply_to(message, 'Привет, чтобы получить данные о карте ЕГКС просто отправь боту ее номер в формате:\n xxx xxx xxx либо xxx xxx (без префикса из 000).')
-        logging.info(f'Said hi to user [{message.from_user.username}] with id: [{message.chat.id}]')
-    
+        bot.reply_to(
+            message, 'Привет, чтобы получить данные о карте ЕГКС просто отправь боту ее номер в формате:\n xxx xxx xxx либо xxx xxx (без префикса из 000).')
+        logging.info(
+            f'Said hi to user [{message.from_user.username}] with id: [{message.chat.id}]')
+
     @bot.message_handler(commands=['help'])
     def help(message):
         bot.reply_to(message, 'Если у вас есть вопросы по боту,вы можете написать разработчику в телеграмме @vlad1k11 или на электронную почту: vlad1k121@yandex.ru.')
-        logging.info(f'Send help message to user [{message.from_user.username}] with id: [{message.chat.id}]')
+        logging.info(
+            f'Send help message to user [{message.from_user.username}] with id: [{message.chat.id}]')
 
     @bot.message_handler(commands=['getcount'])
     def get_chat_members_count(message):
         if (message.from_user.id != AUTHOR_ID):
             return
         members_count = bot.get_chat_members_count(message.chat.id)
-        bot.send_message(AUTHOR_ID, f'There are {members_count} users to used bot.')
+        bot.send_message(
+            AUTHOR_ID, f'There are {members_count} users to used bot.')
 
     @bot.message_handler(commands=['sendmessage'])
     def send_message(message):
@@ -77,7 +91,7 @@ def bot_actions():
         recipient_id = arg[0]
         text = ' '.join(arg[1:])
         bot.send_message(recipient_id, text)
-    
+
     @bot.message_handler(commands=["getuser"])
     def get_user(message):
         if (message.from_user.id != AUTHOR_ID):
@@ -87,44 +101,55 @@ def bot_actions():
         if (len(arg) != 2):
             bot.send_message(AUTHOR_ID, 'not valid command')
             return
-    
+
         user_id = arg[1]
         user_info = bot.get_chat_member(user_id, user_id).user
-        bot.send_message(AUTHOR_ID, "Id: " + str(user_info.id) + "\nFirst Name: " + str(user_info.first_name) + "\nLast Name: " + str(user_info.last_name) +"\nUsername: @" + str(user_info.username))
-    
+        bot.send_message(AUTHOR_ID, "Id: " + str(user_info.id) + "\nFirst Name: " + str(user_info.first_name) +
+                         "\nLast Name: " + str(user_info.last_name) + "\nUsername: @" + str(user_info.username))
+
     @bot.message_handler(content_types=['text'])
     def get_egks_info(message):
-        
+
         card_number = message.text.replace(" ", "")
         chat_id = message.chat.id
-        first_name = message.from_user.first_name 
+        first_name = message.from_user.first_name
         last_name = message.from_user.last_name
         username = message.from_user.username
 
         inline_message = message.text.replace("\n", " | ")
-        logging.info(f'{first_name} {last_name} [{username}] [{chat_id}] send message: [{inline_message}].')
+        logging.info(
+            f'{first_name} {last_name} [{username}] [{chat_id}] send message: [{inline_message}].')
 
         if (not card_number.isdecimal()):
-            logging.info(f'Not valid card number [{card_number}] from user [{chat_id}].')
-            bot.send_message(chat_id=chat_id, text='Номер карты должен состоять только из чисел')
-            return
-        
-        if (len(card_number) != 6 and len(card_number) != 9):
-            logging.info(f'Not valid card number [{card_number}] from user [{chat_id}].')
-            bot.send_message(chat_id=chat_id, text='Номер карты должен состоять из 6 либо 9 символов')
+            logging.info(
+                f'Not valid card number [{card_number}] from user [{chat_id}].')
+            bot.send_message(
+                chat_id=chat_id, text='Номер карты должен состоять только из чисел')
             return
 
-        response = requests.post(f'https://www.egks.ru/card?number={card_number}', verify=False)
+        if (len(card_number) != 6 and len(card_number) != 9):
+            logging.info(
+                f'Not valid card number [{card_number}] from user [{chat_id}].')
+            bot.send_message(
+                chat_id=chat_id, text='Номер карты должен состоять из 6 либо 9 символов')
+            return
+
+        response = requests.post(
+            f'https://www.egks.ru/card?number={card_number}', verify=False)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        result_message = str(soup.find('p')).replace('<br/>', '\n').replace('<p>', '').replace('</p>', '')
+        result_message = str(soup.find('p')).replace(
+            '<br/>', '\n').replace('<p>', '').replace('</p>', '')
         if (len(result_message) == 0):
-            bot.send_message(chat_id=chat_id, text=f'Карта с номером {card_number} не найдена либо неактивна')
+            bot.send_message(
+                chat_id=chat_id, text=f'Карта с номером {card_number} не найдена либо неактивна')
         else:
             bot.send_message(chat_id=chat_id, text=result_message)
 
         result_message = result_message.replace("\n", " | ")
-        logging.info(f'Send message: [{result_message}] to user [{username}] [{chat_id}]')
+        logging.info(
+            f'Send message: [{result_message}] to user [{username}] [{chat_id}]')
+
 
 if __name__ == '__main__':
-    bot_polling()
+    main()
